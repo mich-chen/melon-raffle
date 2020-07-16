d451dc0bd44027e4174b2d1c87d34912a65b142c
Michelle Chen <mich.chen.94@gmail.com>
2020-07-15 21:35:41 -0700

    added condition if __name__==__main__: condition to allow run as script

--- raffle.py
+++ raffle.py
@@ -1,6 +1,6 @@
 """Read customer data from file and run a raffle."""
 
-from random import choice
+import random
 
 
 
@@ -43,7 +43,7 @@ def get_customers_from_file(customer_file_path):
 def pick_winner(customers):
     """Choose a random winner from list of customers."""
 
-    chosen_customer = choice(customers)
+    chosen_customer = random.choice(customers)
 
     print("Tell {name} at {email} that they've won".format(
         name=chosen_customer.name,
@@ -56,3 +56,6 @@ def run_raffle():
 
     customers = get_customers_from_file("customers.txt")
     pick_winner(customers)
+
+if __name__ == "__main__":
+    run_raffle()
